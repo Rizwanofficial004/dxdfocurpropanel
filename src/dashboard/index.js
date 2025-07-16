530b1614e894fcaab90ba12b7ccff7aa6c3facbd
dxdglobal <support@dxdglobal.com>
2025-07-16 12:18:04 +0300

    update

--- src/dashboard/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Dashboard } from './pages/Dashboard';
-export { default as ThemeDemo } from './pages/ThemeDemo';
-export { default as DashboardLayout } from './components/layout/DashboardLayout';
-export { ThemeProvider } from './context/ThemeContext';
-export * from './components';
