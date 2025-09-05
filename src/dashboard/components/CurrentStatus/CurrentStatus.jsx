a106e4a37ac7df02542c6b37f94dc9123097b2c8
Deniz Üstündağ <drivedeluxe1@gmail.com>
2025-09-05 19:36:50 +0300

    update Font-end  app

--- src/dashboard/components/CurrentStatus/CurrentStatus.jsx
+++ src/dashboard/components/CurrentStatus/CurrentStatus.jsx
@@ -8,7 +8,7 @@ const CurrentStatusContainer = styled.div`
   padding: 20px;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1);
   border: 1px solid #e5e7eb;
-  height: fit-content;
+  height: auto;
 `;
 
 const CurrentStatusTitle = styled.h2`
